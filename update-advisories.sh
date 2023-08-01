3dd7421594968cfec7287fa2fe68d0b14c9aa868
Tim te Beek <tim@moderne.io>
2023-08-01 11:31:19 +0200

    Checkout advisory-database to separate folder

--- update-advisories.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-./gradlew build deps
-java -classpath "build/deps/*:build/libs/*" \
-  org.openrewrite.java.dependencies.github.ParseAdvisories \
-  build/advisories \
-  src/main/resources/advisories.csv

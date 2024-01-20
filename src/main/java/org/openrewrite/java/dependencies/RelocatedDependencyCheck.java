c424b81470e7e0138ba495eb41cc8ad34932ee9c
Tim te Beek <tim@moderne.io>
2024-01-20 18:46:32 +0100

    Add visitor implementation for Maven dependencies and plugins

--- src/main/java/org/openrewrite/java/dependencies/RelocatedDependencyCheck.java
+++ src/main/java/org/openrewrite/java/dependencies/RelocatedDependencyCheck.java
@@ -16,20 +16,24 @@
 package org.openrewrite.java.dependencies;
 
 import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import lombok.Value;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.ScanningRecipe;
+import org.openrewrite.Tree;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.StringUtils;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.dependencies.oldgroupids.Migration;
+import org.openrewrite.marker.Markers;
+import org.openrewrite.maven.MavenIsoVisitor;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Content;
+import org.openrewrite.xml.tree.Xml;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RelocatedDependencyCheck extends ScanningRecipe<RelocatedDependencyCheck.Accumulator> {
     @Override
@@ -62,25 +66,85 @@ public class RelocatedDependencyCheck extends ScanningRecipe<RelocatedDependency
 
     @Override
     public Accumulator getInitialValue(ExecutionContext ctx) {
-        Map<GroupArtifact, Relocation> migrations = new HashMap<>();
         try {
-            ObjectReader objectReader = new CsvMapper().readerWithSchemaFor(Migration.class);
-            MappingIterator<Migration> objectMappingIterator = objectReader
+            MappingIterator<Migration> objectMappingIterator = new CsvMapper()
+                    .readerWithSchemaFor(Migration.class)
                     .readValues(RelocatedDependencyCheck.class.getResourceAsStream("/migrations.csv"));
+            Map<GroupArtifact, Relocation> migrations = new HashMap<>();
             while (objectMappingIterator.hasNext()) {
                 Migration def = objectMappingIterator.next();
-                GroupArtifact oldGav = new GroupArtifact(def.getOldGroupId(), StringUtils.isBlank(def.getOldArtifactId())? null : def.getOldArtifactId());
-                GroupArtifact newGav = new GroupArtifact(def.getNewGroupId(), StringUtils.isBlank(def.getNewArtifactId())? null : def.getNewArtifactId());
+                GroupArtifact oldGav = new GroupArtifact(def.getOldGroupId(), StringUtils.isBlank(def.getOldArtifactId()) ? null : def.getOldArtifactId());
+                GroupArtifact newGav = new GroupArtifact(def.getNewGroupId(), StringUtils.isBlank(def.getNewArtifactId()) ? null : def.getNewArtifactId());
                 migrations.put(oldGav, new Relocation(newGav, StringUtils.isBlank(def.getContext()) ? null : def.getContext()));
             }
+            return new Accumulator(migrations);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        return new Accumulator(migrations);
     }
 
     @Override
     public TreeVisitor<?, ExecutionContext> getScanner(Accumulator acc) {
-        return null;
+        return new MavenIsoVisitor<ExecutionContext>() {
+            @Override
+            public @Nullable Xml visit(@Nullable Tree tree, ExecutionContext executionContext) {
+                return null; // Any Maven pom.xml file
+            }
+        };
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor(Accumulator acc) {
+        return mavenVisitor(acc);
+    }
+
+    private static TreeVisitor<?, ExecutionContext> mavenVisitor(Accumulator acc) {
+        final XPathMatcher dependencyMatcher = new XPathMatcher("/project/*/dependencies/dependency");
+        return new MavenIsoVisitor<ExecutionContext>() {
+            @Override
+            public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext executionContext) {
+                tag = super.visitTag(tag, executionContext);
+                Optional<String> optionalGroupId = tag.getChildValue("groupId");
+                Optional<String> optionalArtifactId = tag.getChildValue("artifactId");
+                if (isDependencyTag() || dependencyMatcher.matches(getCursor())) {
+                    if (optionalGroupId.isPresent()) {
+                        String groupId = optionalGroupId.get();
+                        String artifactId = optionalArtifactId.orElse(null);
+                        tag = maybeAddComment(tag, groupId, artifactId);
+                        tag = maybeAddComment(tag, groupId, null);
+                    }
+                } else if (isPluginTag()) {
+                    if (optionalArtifactId.isPresent()) {
+                        String groupId = tag.getChildValue("groupId").orElse("org.apache.maven.plugins");
+                        String artifactId = optionalArtifactId.get();
+                        tag = maybeAddComment(tag, groupId, artifactId);
+                        tag = maybeAddComment(tag, groupId, null);
+                    }
+                }
+                return tag;
+            }
+
+            private Xml.Tag maybeAddComment(Xml.Tag tag, String groupId, @Nullable String artifactId) {
+                Relocation relocation = acc.getMigrations().get(new GroupArtifact(groupId, artifactId));
+                if (relocation != null) {
+                    String commentText = String.format("Relocated to %s%s%s",
+                            relocation.getTo().getGroupId(),
+                            Optional.ofNullable(relocation.getTo().getArtifactId())
+                                    .map(a -> ":" + a)
+                                    .orElse(""),
+                            relocation.getContext() == null ? "" : " as per \"" + relocation.getContext() + "\"");
+                    List<Content> contents = new ArrayList<>(tag.getContent());
+                    boolean containsComment = contents.stream()
+                            .anyMatch((c) -> c instanceof Xml.Comment && commentText.equals(((Xml.Comment) c).getText()));
+                    if (!containsComment) {
+                        int insertPos = 0;
+                        Xml.Comment customComment = new Xml.Comment(Tree.randomId(), contents.get(insertPos).getPrefix(), Markers.EMPTY, commentText);
+                        contents.add(insertPos, customComment);
+                        return tag.withContent(contents);
+                    }
+                }
+                return tag;
+            }
+        };
     }
 }
--- src/test/java/org/openrewrite/java/dependencies/RelocatedDependencyCheckTest.java
+++ src/test/java/org/openrewrite/java/dependencies/RelocatedDependencyCheckTest.java
@@ -15,20 +15,26 @@
  */
 package org.openrewrite.java.dependencies;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.dependencies.RelocatedDependencyCheck.Accumulator;
 import org.openrewrite.java.dependencies.RelocatedDependencyCheck.GroupArtifact;
 import org.openrewrite.java.dependencies.RelocatedDependencyCheck.Relocation;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.maven.Assertions.pomXml;
 
 class RelocatedDependencyCheckTest implements RewriteTest {
     @Test
     void initialValueParser() {
-        Accumulator initialValue = new RelocatedDependencyCheck().getInitialValue(null);
+        Accumulator initialValue = new RelocatedDependencyCheck().getInitialValue(new InMemoryExecutionContext());
         Map<GroupArtifact, Relocation> migrations = initialValue.getMigrations();
         assertThat(migrations)
           .containsEntry(new GroupArtifact("commons-lang", "commons-lang"),
@@ -36,4 +42,169 @@ class RelocatedDependencyCheckTest implements RewriteTest {
           .containsEntry(new GroupArtifact("org.codehaus.groovy", null),
             new Relocation(new GroupArtifact("org.apache.groovy", null), null));
     }
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new RelocatedDependencyCheck());
+    }
+
+    @Nested
+    class Maven {
+        @Test
+        @DocumentExample
+        void findRelocatedMavenDependencies() {
+            rewriteRun(
+              //language=xml
+              pomXml(
+                """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>org.openrewrite.example</groupId>
+                    <artifactId>rewrite-example</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                    <dependencies>
+                      <dependency>
+                        <groupId>commons-lang</groupId>
+                        <artifactId>commons-lang</artifactId>
+                        <version>2.6</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>org.codehaus.groovy</groupId>
+                        <artifactId>groovy</artifactId>
+                        <version>2.5.6</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+                  """,
+                """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>org.openrewrite.example</groupId>
+                    <artifactId>rewrite-example</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                    <dependencies>
+                      <dependency>
+                        <!--Relocated to org.apache.commons:commons-lang3-->
+                        <groupId>commons-lang</groupId>
+                        <artifactId>commons-lang</artifactId>
+                        <version>2.6</version>
+                      </dependency>
+                      <dependency>
+                        <!--Relocated to org.apache.groovy-->
+                        <groupId>org.codehaus.groovy</groupId>
+                        <artifactId>groovy</artifactId>
+                        <version>2.5.6</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+                  """
+              )
+
+            );
+        }
+
+        @Test
+        void findRelocatedMavenPlugins() {
+            rewriteRun(
+              //language=xml
+              pomXml(
+                """
+                  <project>
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>org.openrewrite.example</groupId>
+                      <artifactId>rewrite-example</artifactId>
+                      <version>1.0-SNAPSHOT</version>
+                      <build>
+                        <plugins>
+                          <plugin>
+                            <groupId>org.codehaus.groovy</groupId>
+                            <artifactId>groovy-eclipse-compiler</artifactId>
+                            <version>3.3.0-01</version>
+                          </plugin>
+                        </plugins>
+                      </build>
+                  </project>
+                  """,
+                """
+                  <project>
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>org.openrewrite.example</groupId>
+                      <artifactId>rewrite-example</artifactId>
+                      <version>1.0-SNAPSHOT</version>
+                      <build>
+                        <plugins>
+                          <plugin>
+                            <!--Relocated to org.apache.groovy-->
+                            <groupId>org.codehaus.groovy</groupId>
+                            <artifactId>groovy-eclipse-compiler</artifactId>
+                            <version>3.3.0-01</version>
+                          </plugin>
+                        </plugins>
+                      </build>
+                  </project>
+                  """
+              )
+            );
+        }
+
+        @Test
+        @Disabled("Not supported yet")
+        void findRelocatedMavenPluginDependency() {
+            rewriteRun(
+              //language=xml
+              pomXml(
+                """
+                  <project>
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>org.openrewrite.example</groupId>
+                      <artifactId>rewrite-example</artifactId>
+                      <version>1.0-SNAPSHOT</version>
+                      <build>
+                        <plugins>
+                          <plugin>
+                            <groupId>org.openrewrite.maven</groupId>
+                            <artifactId>org.openrewrite.maven</artifactId>
+                            <version>5.20.0</version>
+                            <dependencies>
+                              <dependency>
+                                <groupId>commons-lang</groupId>
+                                <artifactId>commons-lang</artifactId>
+                                <version>2.6</version>
+                              </dependency>
+                            </dependencies>
+                          </plugin>
+                        </plugins>
+                      </build>
+                  </project>
+                  """,
+                """
+                  <project>
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>org.openrewrite.example</groupId>
+                      <artifactId>rewrite-example</artifactId>
+                      <version>1.0-SNAPSHOT</version>
+                      <build>
+                        <plugins>
+                          <plugin>
+                            <groupId>org.openrewrite.maven</groupId>
+                            <artifactId>org.openrewrite.maven</artifactId>
+                            <version>5.20.0</version>
+                            <dependencies>
+                              <dependency>
+                                <!--Relocated to org.apache.commons:commons-lang3-->
+                                <groupId>commons-lang</groupId>
+                                <artifactId>commons-lang</artifactId>
+                                <version>2.6</version>
+                              </dependency>
+                            </dependencies>
+                          </plugin>
+                        </plugins>
+                      </build>
+                  </project>
+                  """
+              )
+            );
+        }
+    }
+
 }

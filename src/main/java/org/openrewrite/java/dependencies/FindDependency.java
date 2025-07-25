ac271e55bf1be21326a260a22b4d14b5dbb71855
Sam Snyder <sam@moderne.io>
2025-07-25 15:59:24 -0700

    Add a FindDependency recipe which aggregates both the Maven and Gradle variants

---
+++ src/main/java/org/openrewrite/java/dependencies/FindDependency.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Moderne Proprietary. Only for use by Moderne customers under the terms of a commercial contract.
+ */
+package org.openrewrite.java.dependencies;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.*;
+import org.openrewrite.groovy.tree.G;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.kotlin.tree.K;
+import org.openrewrite.xml.tree.Xml;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class FindDependency extends Recipe {
+
+    @Option(displayName = "Group ID",
+            description = "The first part of a dependency coordinate identifying its publisher.",
+            example = "com.google.guava")
+    String groupId;
+
+    @Option(displayName = "Artifact ID",
+            description = "The second part of a dependency coordinate uniquely identifying it among artifacts from the same publisher.",
+            example = "guava")
+    String artifactId;
+
+    @Option(displayName = "Version",
+            description = "An exact version number or node-style semver selector used to select the version number.",
+            example = "3.0.0",
+            required = false)
+    @Nullable
+    String version;
+
+    @Option(displayName = "Version pattern",
+            description = "Allows version selection to be extended beyond the original Node Semver semantics. " +
+                          "So for example, setting 'version' to \"25-29\" can be paired with a metadata pattern of \"-jre\" to select Guava 29.0-jre",
+            example = "-jre",
+            required = false)
+    @Nullable
+    String versionPattern;
+
+    @Option(displayName = "Configuration",
+            description = "For Gradle only, the dependency configuration to search for dependencies in. If omitted then all configurations will be searched.",
+            example = "api",
+            required = false)
+    @Nullable
+    String configuration;
+
+    @Override
+    public String getDisplayName() {
+        return "Find Maven and Gradle dependencies";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Finds direct dependencies declared in Maven and Gradle build files. " +
+               "This does *not* search transitive dependencies. " +
+               "To detect both direct and transitive dependencies use `org.openrewrite.java.dependencies.DependencyInsight`" +               "This recipe works for both Maven and Gradle projects.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new TreeVisitor<Tree, ExecutionContext>() {
+
+            final TreeVisitor<?, ExecutionContext> mavenFindDependency = new org.openrewrite.maven.search.FindDependency(groupId,artifactId,version,versionPattern)
+                    .getVisitor();
+            final TreeVisitor<?, ExecutionContext> gradleFindDependency = new org.openrewrite.gradle.search.FindDependency(groupId, artifactId, configuration)
+                    .getVisitor();
+
+            @Override
+            public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
+                if (!(tree instanceof SourceFile)) {
+                    return tree;
+                }
+                SourceFile s = (SourceFile)tree;
+                if (mavenFindDependency.isAcceptable(s, ctx)) {
+                    return mavenFindDependency.visitNonNull(tree, ctx);
+                } else if (gradleFindDependency.isAcceptable(s, ctx)) {
+                    // Handle Gradle projects
+                    return gradleFindDependency.visitNonNull(tree, ctx);
+                }
+                return s;
+            }
+        };
+    }
+}
---
+++ src/test/java/org/openrewrite/java/dependencies/FindDependencyTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Moderne Proprietary. Only for use by Moderne customers under the terms of a commercial contract.
+ */
+package org.openrewrite.java.dependencies;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
+import static org.openrewrite.maven.Assertions.pomXml;
+
+@SuppressWarnings("GroovyAssignabilityCheck")
+class FindDependencyTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new FindDependency("org.openrewrite", "rewrite-core", null, null, null));
+    }
+
+    @DocumentExample
+    @Test
+    void findMavenDependency() {
+        rewriteRun(
+          //language=xml
+          pomXml(
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>1</version>
+                <dependencies>
+                  <dependency>
+                    <groupId>org.openrewrite</groupId>
+                    <artifactId>rewrite-core</artifactId>
+                    <version>8.0.0</version>
+                  </dependency>
+                </dependencies>
+              </project>
+              """,
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>1</version>
+                <dependencies>
+                  <!--~~>--><dependency>
+                    <groupId>org.openrewrite</groupId>
+                    <artifactId>rewrite-core</artifactId>
+                    <version>8.0.0</version>
+                  </dependency>
+                </dependencies>
+              </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void findGradleDependency() {
+        rewriteRun(
+          spec -> spec.beforeRecipe(withToolingApi()),
+          //language=groovy
+          buildGradle(
+            """
+              plugins {
+                  id 'java-library'
+              }
+
+              repositories {
+                  mavenCentral()
+              }
+
+              dependencies {
+                  api "org.openrewrite:rewrite-core:8.0.0"
+              }
+              """,
+            """
+              plugins {
+                  id 'java-library'
+              }
+
+              repositories {
+                  mavenCentral()
+              }
+
+              dependencies {
+                  /*~~>*/api "org.openrewrite:rewrite-core:8.0.0"
+              }
+              """
+          )
+        );
+    }
+}

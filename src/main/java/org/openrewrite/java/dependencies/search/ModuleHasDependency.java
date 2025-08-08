fd6f26d848228629bd2811a7283ca339c0f956b1
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-08-08 13:35:59 +0200

    add reproducing tests suit for version patterns in `ModuleHasDependencies` and its delegates. Also fix the parameter swap in `ModuleHasDependency`.

--- src/main/java/org/openrewrite/java/dependencies/search/ModuleHasDependency.java
+++ src/main/java/org/openrewrite/java/dependencies/search/ModuleHasDependency.java
@@ -93,7 +93,8 @@ public class ModuleHasDependency extends ScanningRecipe<Set<JavaProject>> {
                 tree.getMarkers()
                         .findFirst(JavaProject.class)
                         .ifPresent(jp -> {
-                            Tree t = new DependencyInsight(groupIdPattern, artifactIdPattern, scope, version)
+                            Tree t = new DependencyInsight(groupIdPattern, artifactIdPattern, version, scope)
+//                            Tree t = new DependencyInsight(groupIdPattern, artifactIdPattern, scope, version)
                                     .getVisitor()
                                     .visit(tree, ctx);
                             if (t != tree) {

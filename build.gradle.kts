93db193fc7925f9b0a9350f1e111525e5b0d0805
Tim te Beek <timtebeek@gmail.com>
2025-10-28 17:08:13 +0000

    Gradle 9.1.0 on Java 25
    Use this link to re-run the recipe: https://app.moderne.io/builder/klr9GvNKg?organizationId=QUxML09wZW4gU291cmNlL09wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ dependencies {
     implementation("org.openrewrite:rewrite-gradle")
     implementation("org.openrewrite:rewrite-groovy")
 
-    runtimeOnly("org.openrewrite:rewrite-java-17")
-
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")

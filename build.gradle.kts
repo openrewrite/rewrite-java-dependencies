3ebc88c5c8b0b86498a75e603c7113e03b3eefbc
Tim te Beek <tim@moderne.io>
2025-10-29 18:41:25 +0100

    Gradle 9.1.0 on Java 21 (#159)
    * Gradle 9.1.0 on Java 25
    
    Use this link to re-run the recipe: https://app.moderne.io/builder/klr9GvNKg?organizationId=QUxML09wZW4gU291cmNlL09wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>
    
    * Add `rewrite-java-25` until provided by `rewrite-test`
    
    * Use `rewrite-java-21` on the test runtime classpath
    
    * Explicitly only install Java 21
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Add `testRuntimeOnly("org.openrewrite:rewrite-java-21")`
    
    * Only install Java 21
    
    * Rely on default installed Java 21
    
    ---------
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ dependencies {
     implementation("org.openrewrite:rewrite-gradle")
     implementation("org.openrewrite:rewrite-groovy")
 
-    runtimeOnly("org.openrewrite:rewrite-java-17")
-
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
@@ -21,6 +19,8 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.openrewrite.gradle.tooling:model:${rewriteVersion}")
     testImplementation(gradleApi())
+
+    testRuntimeOnly("org.openrewrite:rewrite-java-21")
     testRuntimeOnly("com.google.guava:guava:latest.release")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.+")
 }

3a829cbf96012140303b4c02410f37c2bb492fa7
Tim te Beek <tim@moderne.io>
2025-10-29 11:22:04 +0100

    Apply suggestions from code review

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     testImplementation("org.openrewrite.gradle.tooling:model:${rewriteVersion}")
     testImplementation(gradleApi())
 
-    testRuntimeOnly("org.openrewrite:rewrite-java-21")
     testRuntimeOnly("com.google.guava:guava:latest.release")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.+")
 }
